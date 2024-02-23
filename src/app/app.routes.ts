e68085c799d7b82c5ebdaa9107104dd7b075d562
Fraser Hobbs <fraser.hobbs@sky.com>
2024-02-23 19:37:52 +0000

    Created base-layout component, updated routing configuration
    Signed-off-by: Fraser Hobbs <fraser@Frasers-MacBook-Pro.local>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,23 @@
 import {Routes} from '@angular/router';
 import {LoginPageComponent} from "./pages/login-page/login-page.component";
 import {HomePageComponent} from "./pages/home-page/home-page.component";
+import {BaseLayoutComponent} from "./layouts/base-layout/base-layout.component";
 
 export const routes: Routes = [
-  {path: 'login', component: LoginPageComponent},
-  {path: '', component: HomePageComponent}
+
+  // Routes With Navbar
+  {
+    path: '',
+    component: BaseLayoutComponent,
+    children: [
+      {path: '', component: HomePageComponent},
+      {path: 'home', component: HomePageComponent},
+    ],
+  },
+
+  // Plain Routes
+
+  {path: 'login', component: LoginPageComponent}
+
+
 ];
