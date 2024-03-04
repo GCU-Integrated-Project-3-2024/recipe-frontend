5301395b9fd8e9f632920b152888f68013f087c3
Craigmcneil98 <cmcnei301@caledonian.ac.uk>
2024-03-04 12:44:44 +0000

    Added base component for the recipe display.

---
+++ src/app/components/recipe-display/recipe-display.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-display',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipe-display.component.html',
+  styleUrl: './recipe-display.component.scss'
+})
+export class RecipeDisplayComponent {
+
+}
