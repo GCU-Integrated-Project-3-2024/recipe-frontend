dfec63d86fa60fd9a6c2135215d29418380c9dcd
Craigmcneil98 <cmcnei301@caledonian.ac.uk>
2024-03-04 12:43:26 +0000

    Updated app.routes.ts with new route for the recipes page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import {Routes} from '@angular/router';
 import {LoginPageComponent} from "./pages/login-page/login-page.component";
 import {HomePageComponent} from "./pages/home-page/home-page.component";
 import {BaseLayoutComponent} from "./layouts/base-layout/base-layout.component";
+import { RecipePageComponent } from './pages/recipe-page/recipe-page.component';
 
 export const routes: Routes = [
 
@@ -12,6 +13,7 @@ export const routes: Routes = [
     children: [
       {path: '', component: HomePageComponent},
       {path: 'home', component: HomePageComponent},
+      {path: 'recipes', component: RecipePageComponent}
     ],
   },
 
