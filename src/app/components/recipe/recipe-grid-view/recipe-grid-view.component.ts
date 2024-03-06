04dd1289c2bb667cded3a7e62c91188e622f3710
Craigmcneil98 <Cmcnei301@caledonian.ac.uk>
2024-03-06 14:59:26 +0000

    Updated recipe-display, recipe-grid-view, and recipe-list-view components with new HTML and styling.

--- src/app/components/recipe/recipe-grid-view/recipe-grid-view.component.ts
+++ src/app/components/recipe/recipe-grid-view/recipe-grid-view.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Recipe } from '../../../interfaces/recipe.interface';
+import { RecipeCardComponent } from '../recipe-card/recipe-card.component';
 
 @Component({
   selector: 'app-recipe-grid-view',
   standalone: true,
-  imports: [],
+  imports: [RecipeCardComponent],
   templateUrl: './recipe-grid-view.component.html',
   styleUrl: './recipe-grid-view.component.scss'
 })
-export class RecipeGridViewComponent {
 
+export class RecipeGridViewComponent {
+  @Input() recipes!: Recipe[];
 }
--- src/app/components/recipe/recipe-list-view/recipe-list-view.component.ts
+++ src/app/components/recipe/recipe-list-view/recipe-list-view.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {Recipe} from '../../../interfaces/recipe.interface';
 import { StarRatingComponent } from '../star-rating/star-rating.component';
 
 @Component({
@@ -8,12 +9,7 @@ import { StarRatingComponent } from '../star-rating/star-rating.component';
   templateUrl: './recipe-list-view.component.html',
   styleUrl: './recipe-list-view.component.scss'
 })
+
 export class RecipeListViewComponent {
-  recipes = [
-    { id: 1, name: 'Recipe 1', imageUrl: '/assets/images/food/burger.jpeg', rating: 4.5 },
-    { id: 2, name: 'Recipe 2', imageUrl: '/assets/images/food/fishchips.jpg', rating: 3.8 },
-    { id: 3, name: 'Recipe 3', imageUrl: '/assets/images/food/chili.jpg', rating: 2.4 },
-    { id: 4, name: 'Recipe 4', imageUrl: '/assets/images/food/omelette.jpg', rating: 1.2 },
-    { id: 5, name: 'Recipe 5', imageUrl: '/assets/images/food/smoothie.jpg', rating: 0.5 },
-  ];
+  @Input() recipes!: Recipe[];
 }
