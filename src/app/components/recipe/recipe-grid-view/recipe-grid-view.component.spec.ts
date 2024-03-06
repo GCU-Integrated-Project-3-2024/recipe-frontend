be5db30fde0e3e822b7a8c2850917b957fb92bd9
Craigmcneil98 <Cmcnei301@caledonian.ac.uk>
2024-03-06 13:42:07 +0000

    Added grid-view and list-view components to be used by the recipe-display component.

---
+++ src/app/components/recipe/recipe-grid-view/recipe-grid-view.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecipeGridViewComponent } from './recipe-grid-view.component';
+
+describe('RecipeGridViewComponent', () => {
+  let component: RecipeGridViewComponent;
+  let fixture: ComponentFixture<RecipeGridViewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RecipeGridViewComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(RecipeGridViewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/recipe/recipe-grid-view/recipe-grid-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-grid-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipe-grid-view.component.html',
+  styleUrl: './recipe-grid-view.component.scss'
+})
+export class RecipeGridViewComponent {
+
+}
