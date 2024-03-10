73a65588fa6ecc9dff810b267ff651137a5df55d
Craigmcneil98 <Cmcnei301@caledonian.ac.uk>
2024-03-10 21:39:04 +0000

    Adjusted some styling on the modal component, updated recipe-display to use the new modal. Also removed the initial tests from the home-page and added the new "add-recipe" SVG.

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,25 +1,16 @@
 import { Component, ViewChild } from '@angular/core';
 import { RecipeBarComponent } from '../../components/recipe/recipe-bar/recipe-bar.component';
 import { ModalComponent } from '../../components/modal/modal.component';
+import { LoginCardComponent } from '../../components/login-card/login-card.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [RecipeBarComponent, ModalComponent],
+  imports: [RecipeBarComponent, ModalComponent, LoginCardComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
 
 export class HomePageComponent {
-
-  @ViewChild(ModalComponent) modal!: ModalComponent;
-
-  openModal() {
-      this.modal.openModal();
-  }
-
-  closeModal() {
-      this.modal.closeModal();
-  }
   
 }
