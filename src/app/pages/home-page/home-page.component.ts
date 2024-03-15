b225aa0a0934c09a8131e3df5df3b5063102e7ed
Craigmcneil98 <cmcnei301@caledonian.ac.uk>
2024-03-15 11:04:55 +0000

    Updated styling on header and recipe-display components, included header component in the homepage.

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -2,11 +2,12 @@ import { Component, ViewChild } from '@angular/core';
 import { RecipeBarComponent } from '../../components/recipe/recipe-bar/recipe-bar.component';
 import { ModalComponent } from '../../components/modal/modal.component';
 import { LoginCardComponent } from '../../components/login-card/login-card.component';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [RecipeBarComponent, ModalComponent, LoginCardComponent],
+  imports: [RecipeBarComponent, ModalComponent, LoginCardComponent, HeaderComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
