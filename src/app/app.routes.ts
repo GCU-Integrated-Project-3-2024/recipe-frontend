09fc9b2acc635c8c99ca9617e3e9880a2a0f9107
Craigmcneil98 <cmcnei301@caledonian.ac.uk>
2024-03-15 10:39:37 +0000

    Added base component for the discover page and updated routes to include it.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginPageComponent } from "./pages/login-page/login-page.component";
 import { HomePageComponent } from "./pages/home-page/home-page.component";
 import { BaseLayoutComponent } from "./layouts/base-layout/base-layout.component";
 import { RecipePageComponent } from './pages/recipe-page/recipe-page.component';
+import { DiscoverPageComponent } from './pages/discover-page/discover-page.component';
 
 export const routes: Routes = [
 
@@ -13,7 +14,8 @@ export const routes: Routes = [
     children: [
       {path: '', component: HomePageComponent},
       {path: 'home', component: HomePageComponent},
-      {path: 'recipes', component: RecipePageComponent}
+      {path: 'recipes', component: RecipePageComponent},
+      {path: 'discover', component: DiscoverPageComponent}
     ],
   },
 
