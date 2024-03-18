a99a8a10a183721f1a86086e9384a059ac791692
Craigmcneil98 <cmcnei301@caledonian.ac.uk>
2024-03-18 13:19:42 +0000

    Added basic "content" component for displaying simple text within the site. Added this component to the home page for testing.

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -3,11 +3,12 @@ import { RecipeBarComponent } from '../../components/recipe/recipe-bar/recipe-ba
 import { ModalComponent } from '../../components/modal/modal.component';
 import { LoginCardComponent } from '../../components/login-card/login-card.component';
 import { HeaderComponent } from '../../components/header/header.component';
+import { ContentComponent } from '../../components/content/content.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [RecipeBarComponent, ModalComponent, LoginCardComponent, HeaderComponent],
+  imports: [RecipeBarComponent, ModalComponent, LoginCardComponent, HeaderComponent, ContentComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
