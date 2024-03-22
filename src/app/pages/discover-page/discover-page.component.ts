d0a06eb27f528c050e27a4e84ac6bf59713bb1ef
Craigmcneil98 <Cmcnei301@caledonian.ac.uk>
2024-03-22 03:36:58 +0000

    Updated Discover page to allow for API searches with user queries.

--- src/app/pages/discover-page/discover-page.component.ts
+++ src/app/pages/discover-page/discover-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { RecipeApiService } from   '../../services/recipe-api.service';
 import { Recipe } from '../../interfaces/recipe.interface';
+import { RecipeCardComponent } from '../../components/recipe/recipe-card/recipe-card.component';
 
 @Component({
   selector: 'app-discover-page',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, RecipeCardComponent],
   templateUrl: './discover-page.component.html',
   styleUrl: './discover-page.component.scss'
 })
@@ -16,18 +17,34 @@ export class DiscoverPageComponent {
   constructor(private recipeApiService : RecipeApiService) {}
 
   ngOnInit() {
-    this.getRecipes();
+    this.getRecipes('');
   }
 
-  recipes!: string[];
+  recipes: Recipe[] = [];
+  canSearch: boolean = true;
+  lastQuery: string = '';
 
-  getRecipes() {
-    this.recipeApiService.getRecipes('soup')
-      .then(response => {
-        this.recipes = response.data.recipe;
-      })
-      .catch(error => {
-        console.log(error);
-      })
+  async getRecipes(query: string) {
+    try {
+      const response = await this.recipeApiService.getRecipeByName(query);
+      this.recipes = response;
+      console.log(this.recipes);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  onSearch(event: Event) {
+    const query = (event.target as HTMLInputElement).value;
+    this.lastQuery = query;
+    if (this.canSearch) {
+      this.canSearch = false;
+      setTimeout(() => {
+        this.canSearch = true;
+        this.getRecipes(this.lastQuery);
+      }, 1000);
+    } else {
+      console.log('Cannot search yet');
+    }
   }
 }
