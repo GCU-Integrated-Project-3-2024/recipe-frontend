8563a44fd271eeb9ccb3e2b0ac182baeffaa6c56
Craigmcneil98 <Cmcnei301@caledonian.ac.uk>
2024-03-24 02:21:34 +0000

    Minor changes to the recipe-list-view component to allow each recipe to link to it's corresponding recipe page with full details.

--- src/app/components/recipe/recipe-list-view/recipe-list-view.component.ts
+++ src/app/components/recipe/recipe-list-view/recipe-list-view.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import {Recipe} from '../../../interfaces/recipe.interface';
 import { StarRatingComponent } from '../star-rating/star-rating.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list-view',
   standalone: true,
-  imports: [StarRatingComponent],
+  imports: [StarRatingComponent, RouterModule],
   templateUrl: './recipe-list-view.component.html',
   styleUrl: './recipe-list-view.component.scss'
 })
