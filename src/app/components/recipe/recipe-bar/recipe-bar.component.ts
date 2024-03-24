533debb7632dfc5d993eabcffbc49301beec8ca3
Craigmcneil98 <Cmcnei301@caledonian.ac.uk>
2024-03-24 02:43:29 +0000

    Updated home-page component to now display a selection of 10 random recipes within the recipe-bar. Some small styling changes to the recipe-bar.

--- src/app/components/recipe/recipe-bar/recipe-bar.component.ts
+++ src/app/components/recipe/recipe-bar/recipe-bar.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {RecipeCardComponent} from '../recipe-card/recipe-card.component';
+import {Recipe} from '../../../interfaces/recipe.interface';
 
 @Component({
   selector: 'app-recipe-bar',
@@ -10,11 +11,5 @@ import {RecipeCardComponent} from '../recipe-card/recipe-card.component';
 })
 
 export class RecipeBarComponent {
-  recipes = [
-    { id: 1, name: 'Recipe 1', imageUrl: '/assets/images/food/burger.jpeg', rating: 4.5 },
-    { id: 2, name: 'Recipe 2', imageUrl: '/assets/images/food/fishchips.jpg', rating: 3.8 },
-    { id: 3, name: 'Recipe 3', imageUrl: '/assets/images/food/chili.jpg', rating: 2.4 },
-    { id: 4, name: 'Recipe 4', imageUrl: '/assets/images/food/omelette.jpg', rating: 1.2 },
-    { id: 5, name: 'Recipe 5', imageUrl: '/assets/images/food/smoothie.jpg', rating: 0.5 },
-  ];
+  @Input() recipes!: Recipe[];
 }
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,18 +1,34 @@
 import { Component, ViewChild } from '@angular/core';
 import { RecipeBarComponent } from '../../components/recipe/recipe-bar/recipe-bar.component';
-import { ModalComponent } from '../../components/modal/modal.component';
-import { LoginCardComponent } from '../../components/login-card/login-card.component';
 import { HeaderComponent } from '../../components/header/header.component';
 import { ContentComponent } from '../../components/content/content.component';
+import { RecipeApiService } from '../../services/recipe-api.service';
+import { Recipe } from '../../interfaces/recipe.interface';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [RecipeBarComponent, ModalComponent, LoginCardComponent, HeaderComponent, ContentComponent],
+  imports: [RecipeBarComponent, HeaderComponent, ContentComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
 
 export class HomePageComponent {
   
+  constructor(private recipeApiService: RecipeApiService) { }
+
+  recipes: Recipe[] = [];
+
+  ngOnInit() {
+    this.getRecipes();
+  }
+
+  async getRecipes() {
+    try {
+      const response = await this.recipeApiService.getRandomRecipes();
+      this.recipes = response;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
