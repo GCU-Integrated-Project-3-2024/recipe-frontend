eae9e5b24738efbd3595fba7b9bc734c29a052d9
callux <calluxcarlyle@gmail.com>
2024-04-08 13:35:46 +0100

    Added register card component, imported it to login-page for later

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
 import { LoginCardComponent } from '../../components/login-card/login-card.component';
+import { RegisterCardComponent } from '../../components/register-card/register-card.component';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [LoginCardComponent],
+  imports: [LoginCardComponent, RegisterCardComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
