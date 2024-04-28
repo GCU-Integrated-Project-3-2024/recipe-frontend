dd71011cb7058f421dee0dd5c44919882c75dd2c
Craigmcneil98 <Cmcnei301@caledonian.ac.uk>
2024-04-28 18:00:35 +0100

    Small changes to recipe interface for compatibility with back-end API.

--- src/app/interfaces/recipe.interface.ts
+++ src/app/interfaces/recipe.interface.ts
@@ -1,8 +1,9 @@
 export interface Recipe {
-    id: number;
+    id?: number;
     name: string;
     instructions?: string[];
     ingredients?: string[];
+    numberOfServings?: number;
     youtubeUrl?: string;
     imageUrl: string;
     rating: number;
